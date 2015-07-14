f90a06020d8c9a6e10af5a9d5f1ca7d4fa2759c1
test <wildson2585@gmail.com>
2015-07-14 17:19:52 +0300

    upd.

--- app.js
+++ app.js
@@ -7,18 +7,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(1337, function () {
+var port = Number(process.env.PORT || 1337);
+
+app.listen(port, function () {
   console.log('port 1337');
 });
+
+
 app.get('/', function (req, res) {
   res.render('index',
       { title : 'Содержание' }
-  )
+  );
 });
 app.get('/about', function (req, res) {
   res.render('about',
       { title : 'О практиканте' }
-  )
+  );
 });
 
 
