372c95636699653d1ffdb005475d5ffe7f0bc053
test <wildson2585@gmail.com>
2015-07-16 20:29:48 +0300

    Heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,27 @@
-ALL_TASKS = ['jade', 'express:dev', 'watch', 'connect',];
+ALL_TASKS = ['uglify'];
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        jade: {
-            main: {
-                src: [
-                    'views/*.jade'
-                ],
-                dest: 'build/',
-                expand: true,
-                ext: ".html"
-            }
-        },
-        connect: {
-            server: {
-                options: {
-
-                }
-            }
-
-        },
-
-        watch: {
-            express: {
-                files:  [
-                    'views/**/*.jade',
-                    'public/**/*.css'
-                ],
-                tasks:  ALL_TASKS,
-                options: {
-
-                    spawn: false
-                }
-            }
-        },
-
-        uglify: {
-            main: {
-                files: {
-                    'build/scripts.min.js': '<%= concat.main.dest %>'
-                }
-            }
-        },
-        express: {
-            dev:{
-                options: {
-                    port: 1337,
-                    script: 'app.js'
-                }
-            }
-        }
-
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'app.js',
+        dest: 'build/app.min.js'
+      }
+    },
+    express: {
+        dev:{
+            options: {
+                port: 1337,
+                  script: 'app.js'
+             }
+         }
+     }
     });
-
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks("grunt-contrib-jade");
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-express-server');
-    grunt.loadNpmTasks('grunt-contrib-connect')
-    grunt.loadNpmTasks('grunt-contrib-watch' );
-
-
-
-
     grunt.registerTask('default', ALL_TASKS);
 };
